d9453a1e28cc5fb58c91e2da11fcd45eae306837
wanglian <wanglian1024@gmail.com>
2009-01-02 11:16:05 +0800

    update helper

--- lib/table_header/app/controllers/application_controller.rb
+++ lib/table_header/app/controllers/application_controller.rb
@@ -16,10 +16,10 @@ module TableHeader
             acceptable_columns = join_array_and_hash_keys(args, mappings)
             
             define_method(:select_conditions) do |*default| 
-              conditions = []
+              conditions = {}
               params.keys.each do |name|
                 if acceptable_columns.include?(name) && !params[name].blank?
-                  conditions << ["#{name} = '#{params[name]}'"]
+                  conditions.merge! name => inspect_param(params[name])
                 end
               end
               conditions
@@ -31,10 +31,10 @@ module TableHeader
             acceptable_columns = join_array_and_hash_keys(args, mappings)
             
             define_method(:filter_conditions) do |*default| 
-              conditions = []
+              conditions = {}
               params.keys.each do |name|
                 if acceptable_columns.include?(name) && !params[name].blank?
-                  conditions << ["#{name.downcase} LIKE '%#{params[name]}%'"]
+                  conditions.merge! name => params[name].downcase
                 end
               end
               conditions
@@ -44,7 +44,15 @@ module TableHeader
         end
 
         module InstanceMethods
+          def header_conditions
+            select_conditions.merge(filter_conditions)
+          end
           
+          def inspect_param(value)
+            eval value
+          rescue NameError => e
+            value
+          end
         end
       end
     end
